fdd9e6e3ddff2ad6de64343991761dd598cd42c5
Alex Marchant <alexjmarchant@gmail.com>
2015-08-16 19:06:49 -0400

    aws publish WIP

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var sass       = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var webserver  = require('gulp-webserver');
 var awspublish = require('gulp-awspublish');
+var merge      = require('merge-stream');
+var rename     = require("gulp-rename");
+var fs         = require('fs');
 
 
 gulp.task('watch', function() {
@@ -28,13 +31,16 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('publish', function() {
+  var credentials = JSON.parse(fs.readFileSync('aws-credentials.json', 'utf8'));
 
   // create a new publisher using S3 options
   // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
   var publisher = awspublish.create({
     params: {
-      Bucket: 'www.plugformac.com/plug2sitetest/'
-    }
+      Bucket: 'www.plugformac.com'
+    },
+    accessKeyId: credentials.accessKeyId,
+    secretAccessKey: credentials.secretAccessKey
   });
 
   // define custom headers
@@ -42,10 +48,14 @@ gulp.task('publish', function() {
     'Cache-Control': 'max-age=315360000, no-transform, public'
   };
 
-  var gzip = gulp.src(['./index.html', './plug.css', './plug.css.map']).pipe(awspublish.gzip());
+  var gzip = gulp.src(['./*.html', './*.css', './*.css.map']).pipe(awspublish.gzip());
   var plain = gulp.src(['./images/*.png']);
 
-  return gulp.src(gzip, plain)
+  return merge(gzip, plain)
+    .pipe(rename(function (path) {
+      path.dirname += '/plug2sitetest';
+    }))
+
     // publisher will add Content-Length, Content-Type and headers specified above
     // If not specified it will set x-amz-acl to public-read by default
     .pipe(publisher.publish(headers))
