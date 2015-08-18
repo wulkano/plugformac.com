3a1ecf854e5faef27146d21089e64bbf3062aaf5
Alex Marchant <alexjmarchant@gmail.com>
2015-08-18 13:07:12 -0400

    Rebound.js trial

--- js/src/modal.js
+++ js/src/modal.js
@@ -1,11 +1,44 @@
-var modal = document.getElementById("modal");
+var modal; 
+var modalContent;
+var shouldHide = false;
+
+// create a SpringSystem and a Spring with a bouncy config.
+var springSystem = new rebound.SpringSystem();
+var spring = springSystem.createSpring(35, 5);
+
+// Add a listener to the spring. Every time the physics
+// solver updates the Spring's value onSpringUpdate will
+// be called.
+spring.addListener({
+  onSpringUpdate: function(spring) {
+    var val = spring.getCurrentValue();
+    val = rebound.MathUtil.mapValueInRange(val, 0, 1, 1, 0.5);
+    scale(modalContent, val);
+
+    console.log(val)
+    if (val <= 0.5 && shouldHide === true && modal.className === "show") {
+      modal.className = ""
+      shouldHide = false
+    }
+  }
+});
+
+// Helper for scaling an element with css transforms.
+function scale(el, val) {
+  el.style.mozTransform =
+  el.style.msTransform =
+  el.style.webkitTransform =
+  el.style.transform = 'scale3d(' + val + ', ' + val + ', 1)';
+}
 
 function showModal() {
   modal.className = "show"
+  spring.setEndValue(0);
 }
 
 function hideModal() {
-  modal.className = ""
+  shouldHide = true;
+  spring.setEndValue(1);
 }
 
 function preventClickthrough(event) {
@@ -14,8 +47,12 @@ function preventClickthrough(event) {
 
 module.exports = {
   init: function() {
+    modal = document.getElementById("modal");
+    modalContent = document.getElementById("modal-content");
     document.getElementById("help-out-button").addEventListener("click", showModal);
     document.getElementById("modal-container").addEventListener("click", hideModal);
-    document.getElementById("modal-content").addEventListener("click", preventClickthrough);
+    modalContent.addEventListener("click", preventClickthrough);
+
+    spring.setCurrentValue(1, true);
   }
 };
