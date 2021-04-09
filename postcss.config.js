f29907e012f24b203d753672639d3349c3fa935d
timothyis <hello@timothy.is>
2021-04-09 23:44:58 +0100

    Enable JIT (woops)

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  // postcss.config.js
+  module.exports = {
+    plugins: {
+      '@tailwindcss/jit': {},
+      autoprefixer: {},
+    }
+  }
