c32b4f0d2fdee85ccea4749578b795f672fd8a03
Tristan Scaglia <Scglia@users.noreply.github.com>
2024-01-18 15:23:52 +0100

    Tailwind CSS tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
